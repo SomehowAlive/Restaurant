78c077791607697254bb02bc5786c3022b0311ac
somehowalive <slow.optimism@gmail.com>
2023-02-19 22:45:26 +0100

    fixed name

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,29 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  entry: { index: "./src/index.js", header: "./src/header.js", home: "./src/home.js" },
-  devtool: "inline-source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Development",
-    }),
-  ],
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
+    mode: "development",
+    entry: { index: "./src/index.js", header: "./src/header.js", home: "./src/home.js" },
+    devtool: "inline-source-map",
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Classy Restuarant",
+        }),
     ],
-  },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
 };

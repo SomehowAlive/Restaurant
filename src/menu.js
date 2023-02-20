07adfe0623fa56757414c003e5d66ebf4eebb733
somehowalive <slow.optimism@gmail.com>
2023-02-20 19:43:40 +0100

    fixed images loading problem

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,21 @@
 import foods from "./assets/food.json";
 import "./styles/menu.css";
+import food1 from "./assets/food1.png";
+import food2 from "./assets/food2.png";
+import food3 from "./assets/food3.png";
+import food4 from "./assets/food4.png";
+import food5 from "./assets/food5.png";
+import food6 from "./assets/food6.png";
+import food7 from "./assets/food7.png";
+import food8 from "./assets/food8.png";
+import food9 from "./assets/food9.png";
+import food10 from "./assets/food10.png";
+import food11 from "./assets/food11.png";
+import food12 from "./assets/food12.png";
+import food13 from "./assets/food13.png";
+
+const imgs = [food1, food2, food3, food4, food5, food6, food7, food8, food9, food10, food11, food12, food13];
+console.log(imgs);
 
 export default function menu(foodType = "starter") {
     const menuContainer = document.createElement("div");
@@ -83,7 +99,7 @@ function createCard(obj) {
         ingredientElem.append(ingredientElemText);
         ingredients.appendChild(ingredientElem);
     }
-    img.src = `../src/assets/${obj.pic}`;
+    img.src = imgs[obj.index - 1];
     price.textContent = obj.price;
 
     card.style.backgroundColor = obj.bgColor;
